756222b4e1f2ffe0ecc9858122220304b4c6f2c3
Gandalf- <austin.voecks@gmail.com>
2018-10-27 20:10:36 -0700

    reformat devbot files

--- devbot/src/bot.hs
+++ devbot/src/bot.hs
@@ -26,129 +26,126 @@ startingState = map (\ event -> Task event Nothing 0)
 
 
 runner :: Integer -> State -> IO State
+-- ^ check each Task for something to do
+-- run for n iterations before dropping out so main can refetch the
+-- events and start us again
 runner runs state =
-    -- check each Task for something to do
-    -- run for n iterations before dropping out so main can refetch the
-    -- events and start us again
+        if runs > minRunsToRestart && noRunners state
+          then return []
+          else do
+              threadDelay $ 1 * second
+              mapM handle state >>= runner (runs + 1)
 
-    if runs > minRunsToRestart && noRunners state
-      then return []
-      else do
-          threadDelay $ 1 * second
-          mapM handle state >>= runner (runs + 1)
+    where
+        second  = 1000000
+        minRunsToRestart = 60 * 5
 
-    where second  = 1000000
-          minRunsToRestart = 60 * 5
-
-          noRunners :: State -> Bool
-          noRunners []                       = True
-          noRunners (Task _ Nothing  _ : xs) = noRunners xs
-          noRunners (Task _ (Just _) _ : _ ) = False
+        noRunners :: State -> Bool
+        noRunners []                       = True
+        noRunners (Task _ Nothing  _ : xs) = noRunners xs
+        noRunners (Task _ (Just _) _ : _ ) = False
 
 
 handle :: Task -> IO Task
 handle task@(Task (Event _ _ d) Nothing _) = do
-    -- not currently running
-    time <- getTime
+        -- not currently running
+        time <- getTime
 
-    if ready time d
-        then check task
-        else return task
+        if ready time d
+            then check task
+            else return task
     where
         ready :: Integer -> Data -> Bool
         ready now (Data _ when _) = now > when
 
 handle task@(Task _ (Just h) _) = do
-    code <- getProcessExitCode h
-    case code of
-        -- still running
-        Nothing          -> return task
+        code <- getProcessExitCode h
+        case code of
+            -- still running
+            Nothing          -> return task
 
-        -- finished
-        Just ExitSuccess -> success task
-        Just _           -> failure task
+            -- finished
+            Just ExitSuccess -> success task
+            Just _           -> failure task
 
 
 check :: Task -> IO Task
 check task@(Task (Event n c d) p s) = do
-    -- if we can't run, wait 30 seconds before trying again
-
-    now <- getTime
-    met <- requirementsMet n c
-    if met
-        then run task
-        else return $ Task (Event n c (backoff now d)) p s
-
+        -- if we can't run, wait 30 seconds before trying again
+        now <- getTime
+        met <- requirementsMet n c
+        if met
+            then run task
+            else return $ Task (Event n c (backoff now d)) p s
     where
-          backoff :: Integer -> Data -> Data
-          backoff now (Data d _ e) = Data d (now + 30) e
+        backoff :: Integer -> Data -> Data
+        backoff now (Data d _ e) = Data d (now + 30) e
 
 
 run :: Task -> IO Task
 run (Task event@(Event _ (Config c _ _) _) _ _) = do
-    -- start running the actions, add handle to Task
-    -- putStrLn $ "Running: " ++ n ++ ":\n" ++ cmd
-
-    h <- spawnCommand cmd
-    Task event (Just h) <$> getTime
-
-    where cmd = intercalate "\n" c
+        -- start running the actions, add handle to Task
+        -- putStrLn $ "Running: " ++ n ++ ":\n" ++ cmd
+        h <- spawnCommand cmd
+        Task event (Just h) <$> getTime
+    where
+        cmd = intercalate "\n" c
 
 
 success :: Task -> IO Task
 success (Task event@(Event _ c _) _ startTime) = do
-    -- the command succeeded, set errors to Nothing, and determine next
-    -- time to run
-    duration <- negate . (startTime -) <$> getTime
+        -- the command succeeded, set errors to Nothing, and determine next
+        -- time to run
+        duration <- negate . (startTime -) <$> getTime
 
-    let newEvent = clearErrors $ updateTime event next duration
-        newTask  = Task newEvent Nothing 0
+        let newEvent = clearErrors $ updateTime event next duration
+            newTask  = Task newEvent Nothing 0
 
-    flush newEvent
-    return newTask
+        flush newEvent
+        return newTask
+    where
+        next = nextRun startTime c
 
-    where next = nextRun startTime c
-
-          clearErrors :: Event -> Event
-          clearErrors (Event n co (Data du wh _)) =
-            Event n co (Data du wh Nothing)
+        clearErrors :: Event -> Event
+        clearErrors (Event n co (Data du wh _)) =
+          Event n co (Data du wh Nothing)
 
 
 failure :: Task -> IO Task
 failure (Task event@(Event n _ d) _ startTime) = do
-    -- the command failed, log the error, increment errors and set next
-    -- time to retry
-    logger $ concat ["running ", n
-                    , " failed, backing off "
-                    , show backoff, " seconds"]
+        -- the command failed, log the error, increment errors and set next
+        -- time to retry
+        logger $ concat ["running ", n
+                        , " failed, backing off "
+                        , show backoff, " seconds"]
 
-    -- next <- getTime >>= (return . (+ 1))
-    next <- (+ backoff) <$> getTime
-    duration <- negate . (startTime -) <$> getTime
+        -- next <- getTime >>= (return . (+ 1))
+        next <- (+ backoff) <$> getTime
+        duration <- negate . (startTime -) <$> getTime
 
-    let newEvent = incrementError $ updateTime event next duration
-        newTask  = Task newEvent Nothing 0
+        let newEvent = incrementError $ updateTime event next duration
+            newTask  = Task newEvent Nothing 0
 
-    flush newEvent
-    return newTask
+        flush newEvent
+        return newTask
+    where
+        backoff = getBackoff d
 
-    where backoff = getBackoff d
+        getBackoff :: Data -> Integer
+        getBackoff (Data _ _ Nothing)  = 10
+        getBackoff (Data _ _ (Just e)) = (e + 1) * 10
 
-          getBackoff :: Data -> Integer
-          getBackoff (Data _ _ Nothing)  = 10
-          getBackoff (Data _ _ (Just e)) = (e + 1) * 10
+        incrementError :: Event -> Event
+        incrementError (Event n c (Data du wh Nothing)) =
+            Event n c (Data du wh (Just 1))
 
-          incrementError :: Event -> Event
-          incrementError (Event n c (Data du wh Nothing)) =
-              Event n c (Data du wh (Just 1))
-
-          incrementError (Event n c (Data du wh (Just e))) =
-              Event n c (Data du wh (Just $ e + 1))
+        incrementError (Event n c (Data du wh (Just e))) =
+            Event n c (Data du wh (Just $ e + 1))
 
 
 updateTime :: Event -> Integer -> Integer -> Event
 updateTime (Event n c (Data _ _ e)) newTime duration =
-  Event n c (Data duration newTime e)
+        Event n c (Data duration newTime e)
 
 
 nextRun :: Integer -> Config -> Integer
@@ -161,35 +158,35 @@ flush (Event n _ d) = set' ["devbot", "data", n] d
 
 logger :: String -> IO ()
 logger msg = do
-    time <- getTime
-    putStrLn $ "devbot: " ++ show time ++ " " ++ msg
+        time <- getTime
+        putStrLn $ "devbot: " ++ show time ++ " " ++ msg
 
 
 requirementsMet :: String -> Config -> IO Bool
 requirementsMet _ (Config _ _ Nothing) = return True
 requirementsMet n (Config _ _ (Just r)) = do
-    req <- get' ["devbot", "requirements", r]
+        req <- get' ["devbot", "requirements", r]
 
-    case req of
-        Nothing -> do
-            logger doesntExist
-            return False
-        (Just a) -> runCheck a
+        case req of
+            Nothing -> do
+                logger doesntExist
+                return False
+            (Just a) -> runCheck a
     where
-          runCheck :: String -> IO Bool
-          runCheck cmd = do
-              code <- spawnCommand cmd >>= waitForProcess
-              case code of
-                  ExitSuccess -> return True
-                  _           -> do
-                      logger cmdFailed
-                      return False
+        runCheck :: String -> IO Bool
+        runCheck cmd = do
+            code <- spawnCommand cmd >>= waitForProcess
+            case code of
+                ExitSuccess -> return True
+                _           -> do
+                    logger cmdFailed
+                    return False
 
-          doesntExist =
-            n ++ " references requirement that doesn't exist"
+        doesntExist =
+          n ++ " references requirement that doesn't exist"
 
-          cmdFailed =
-            "requirement " ++ r ++ " for " ++ n ++ " not met"
+        cmdFailed =
+          "requirement " ++ r ++ " for " ++ n ++ " not met"
 
 
 getTime :: IO Integer
@@ -198,10 +195,11 @@ getTime = round <$> getPOSIXTime
 
 main :: IO ()
 main = do
-    putStrLn "devbot starting up"
-    hSetBuffering stdout LineBuffering
+        putStrLn "devbot starting up"
+        hSetBuffering stdout LineBuffering
 
-    loop
-    where loop = do
-            _ <- events >>= runner 1 . startingState
-            loop
+        loop
+    where
+        loop = do
+          _ <- events >>= runner 1 . startingState
+          loop
--- devbot/src/list.hs
+++ devbot/src/list.hs
@@ -6,67 +6,9 @@ import           Devbot
 import           Data.List             (intercalate)
 import           Data.Time.Clock.POSIX (getPOSIXTime)
 
-now :: IO Integer
-now = round <$> getPOSIXTime
 
-
-blue   = (Blue,   NoColor, Null)
-black  = (White,  NoColor, Null)
-green  = (Green,  NoColor, Bold)
-yellow = (Yellow, NoColor, Null)
-red    = (Red,    NoColor, Null)
-cyan   = (Cyan,   NoColor, Null)
-
-
-printAction :: Config -> String
-printAction (Config a _ _ )  = decorate action blue
-    where action = "    " ++ intercalate pad a
-          pad    = "\n    "
-
-
-printName :: String -> String
-printName name = decorate name green
-
-
-prettyTime :: Integer -> String
-prettyTime i
-        | i <= minute = show i ++ " seconds"
-        | i <= hour   = show (div i minute) ++ " minutes"
-        | i <= day    = show (div i hour) ++ " hours"
-        | otherwise   = show (div i day) ++ " days"
-    where day = 86400
-          hour = 3600
-          minute = 60
-
-
-printInterval :: Config -> String
-printInterval (Config _ i _) =
-        decorate ("    every " ++ prettyTime i) cyan
-
-
-printNext :: Data -> Integer -> String
-printNext (Data _ w _) time
-        | w - time > 0 = decorate ("next in " ++ t) yellow
-        | otherwise    = decorate "now" yellow
-    where t      = prettyTime $ w - time
-
-
-printOptional :: Config -> Data -> String
-printOptional (Config _ _ req) (Data d _ errs) =
-        concat [ maybe "" printErrors errs
-               , printDuration d
-               , maybe "" printRequire req
-               ]
-    where
-          printErrors :: Integer -> String
-          printErrors e = ", " ++ decorate (show e ++ " errors") red
-
-          printDuration :: Integer -> String
-          printDuration d =
-                ", " ++ decorate ("took " ++ show d ++ " seconds") cyan
-
-          printRequire :: String -> String
-          printRequire r = ", requires " ++ r
+main :: IO ()
+main = events >>= mapM_ printEvent
 
 
 printEvent :: Event -> IO ()
@@ -81,5 +23,69 @@ printEvent (Event n c d) = do
             , "\n"
             ]
 
-main :: IO ()
-main = events >>= mapM_ printEvent
+
+printName :: String -> String
+printName name = decorate name green
+
+
+printAction :: Config -> String
+printAction (Config a _ _ )  =
+        decorate action blue
+    where
+        action = "    " ++ intercalate pad a
+        pad    = "\n    "
+
+
+printInterval :: Config -> String
+printInterval (Config _ i _) =
+        decorate ("    every " ++ prettyTime i) cyan
+
+
+printOptional :: Config -> Data -> String
+printOptional (Config _ _ req) (Data d _ errs) =
+        concat [ maybe "" printErrors errs
+               , printDuration d
+               , maybe "" printRequire req
+               ]
+    where
+        printErrors :: Integer -> String
+        printErrors e = ", " ++ decorate (show e ++ " errors") red
+
+        printDuration :: Integer -> String
+        printDuration d =
+            ", " ++ decorate ("took " ++ show d ++ " seconds") cyan
+
+        printRequire :: String -> String
+        printRequire r = ", requires " ++ r
+
+
+printNext :: Data -> Integer -> String
+printNext (Data _ w _) time
+        | w - time > 0 = decorate ("next in " ++ t) yellow
+        | otherwise    = decorate "now" yellow
+    where
+        t = prettyTime $ w - time
+
+
+prettyTime :: Integer -> String
+prettyTime i
+        | i <= minute = show i ++ " seconds"
+        | i <= hour   = show (div i minute) ++ " minutes"
+        | i <= day    = show (div i hour) ++ " hours"
+        | otherwise   = show (div i day) ++ " days"
+    where
+        day = 86400
+        hour = 3600
+        minute = 60
+
+
+now :: IO Integer
+now = round <$> getPOSIXTime
+
+
+blue   = (Blue,   NoColor, Null)
+black  = (White,  NoColor, Null)
+green  = (Green,  NoColor, Bold)
+yellow = (Yellow, NoColor, Null)
+red    = (Red,    NoColor, Null)
+cyan   = (Cyan,   NoColor, Null)
