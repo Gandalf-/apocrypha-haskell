e3de0620ce458512b13d7b85966a150176a9e6f9
Gandalf- <austin.voecks@gmail.com>
2019-01-13 15:10:12 -0800

    update to d

--- src/d.hs
+++ src/d.hs
@@ -2,12 +2,24 @@ module Main where
 
 import           Apocrypha.Protocol (client, defaultContext)
 import           System.Environment (getArgs)
+import           System.IO          (hIsTerminalDevice, stdin)
 
 
 main :: IO ()
 main = do
         c <- defaultContext
-        getArgs >>= client c >>= display
+        arguments >>= client c >>= display
+
+
+arguments :: IO [String]
+-- ^ if data is piped in, use it as the final argument
+arguments = do
+        terminal <- hIsTerminalDevice stdin
+        if terminal
+          then getArgs
+          else do
+            content <- getContents
+            (<> [content]) <$> getArgs
 
 
 display :: Maybe String -> IO ()
