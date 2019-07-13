a95419ca697bc9c05a4401163d9a0fcbf36d3eda
Gandalf- <austin.voecks@gmail.com>
2019-07-13 09:53:32 -0700

    rewrite server with STM

--- src/server.hs
+++ src/server.hs
@@ -1,34 +1,40 @@
 {-# LANGUAGE CPP               #-}
 {-# LANGUAGE OverloadedStrings #-}
 
+import           Control.Monad.STM
 import           Control.Concurrent
+import           Control.Concurrent.STM.TVar
 import           Control.Concurrent.Async (async)
 import           Control.Monad.Except
-import           Control.Monad.Reader
 import           Data.Aeson               (Value (..))
 import           Data.ByteString.Char8    (ByteString)
 import           Data.Text                (Text)
 import qualified Data.Text                as T
 import           Data.Text.Encoding       (decodeUtf8, encodeUtf8)
 import           Network
-import           System.Directory         (doesFileExist, removeFile)
 import           System.Environment       (getArgs)
 import           System.Exit              (die)
 import           System.IO
+#ifndef mingw32_HOST_OS
+import           System.Directory         (doesFileExist, removeFile)
+#endif
 
 import           Apocrypha.Database       (Query, defaultDB, getDB, runAction,
                                            saveDB)
 import           Apocrypha.Internal.Cache (Cache, emptyCache, get, put)
 import           Apocrypha.Options
+#ifndef mingw32_HOST_OS
 import           Apocrypha.Protocol       (defaultTCPPort, protoRead, protoSend,
                                            unixSocketPath)
+#else
+import           Apocrypha.Protocol       (defaultTCPPort, protoRead, protoSend)
+#endif
 
 
-type WriteNeeded = MVar Bool
-type Database = MVar Value
-type DbCache  = MVar Cache
-type ClientCount = MVar Integer
-type ServerApp = ReaderT ThreadData IO
+type WriteNeeded = TVar Bool
+type Database = TVar Value
+type DbCache  = TVar Cache
+type ClientCount = TVar Integer
 
 data ThreadData = ThreadData
         { _threadHandle :: !Handle
@@ -61,13 +67,14 @@ main = do
             tcpSocket  <- listenOn $ PortNumber $ _tcpPort options
 
             putStrLn "Server started"
-            dbMV <- newMVar db
-            wrMV <- newMVar False
-            chMV <- newMVar emptyCache
-            ccMV <- newMVar 0
+            dbMV <- newTVarIO db          -- in memory database
+            chMV <- newTVarIO emptyCache  -- in memory cache
+            wrMV <- newTVarIO False       -- do we have writes to flush to disk?
+            ccMV <- newTVarIO 0           -- running client total
 
+            let env = ThreadData stdout dbMV wrMV ccMV chMV options
             when (_enablePersist options) $
-                persistThread (ThreadData stdout dbMV wrMV ccMV chMV options)
+                persistThread env
 
 #ifndef mingw32_HOST_OS
             unixSocket <- getUnixSocket
@@ -80,7 +87,7 @@ main = do
             clientForker tcpSocket dbMV wrMV ccMV chMV options
 
         persistThread :: ThreadData -> IO ()
-        persistThread = void . async . runReaderT diskWriter
+        persistThread = void . async . diskWriter
 
 #ifndef mingw32_HOST_OS
         getUnixSocket :: IO Socket
@@ -102,130 +109,110 @@ clientForker :: Socket
     -> IO b
 -- ^ listen for clients, fork off workers
 clientForker socket d w n c o = forever $ do
-        count <- readMVar n
-        if count < maxClients
-            then do
-                (h, _, _) <- accept socket
-                hSetBuffering h NoBuffering
+        atomically $ do
+            count <- readTVar n
+            when (count > maxClients) retry
 
-                _count <- takeMVar n
-                putMVar n (_count + 1)
+        -- accept a new client connection, set buffering, increment client total
+        (h, _, _) <- accept socket
+        hSetBuffering h NoBuffering
+        atomically $ modifyTVar n (+ 1)
 
-                forkerThread (ThreadData h d w n c o)
-
-            else threadDelay tenthSecond
+        -- start client loop to handle queries
+        void . async . clientLoop $ ThreadData h d w n c o
     where
-        cacheEnabled = _enableCache o
-        logEnabled   = _enableLog o
-        forkerThread = void . async . runReaderT (clientLoop cacheEnabled logEnabled)
         maxClients   = 500
-        tenthSecond  = 100000
 
 
-diskWriter :: ServerApp ()
+diskWriter :: ThreadData -> IO ()
 -- ^ checks if a write to disk is necessary once per second
 -- done in a separate thread so client threads can run faster
-diskWriter = forever $ do
-        write <- readMVarT =<< viewWrite
-        db <- readMVarT =<< viewDatabase
-        path <- _databasePath <$> viewOptions
+diskWriter (ThreadData _ d w _ _ o) = forever $ do
 
-        liftIO $ threadDelay oneSecond
-        when write $ liftIO (saveDB path db)
+        threadDelay oneSecond
+        needWrite <- readTVarIO w
+
+        when needWrite $ do
+            db <- readTVarIO d
+            saveDB path db
     where
         oneSecond = 1000000
+        path = _databasePath o
 
 
-clientLoop :: Bool -> Bool -> ServerApp ()
+clientLoop :: ThreadData -> IO ()
 -- ^ read queries from the client, serve them or quit
-clientLoop cacheEnabled logEnabled =
-        flip catchError (const cleanUp) $ do
-              query <- getQuery
-              case query of
-                    Nothing  -> cleanUp
+clientLoop td = do
+        query <- protoRead client
+        case query of
+            (Just q) -> do
+                -- run the query through the database, reply to the client, wait for
+                -- another query
+                serve td q
+                clientLoop td
 
-                    (Just q) -> do
-                        handle q
-                        clientLoop cacheEnabled logEnabled
+            Nothing  -> do
+                -- close the connection, decrement total clients
+                hClose client
+                atomically $
+                    modifyTVar (_clientCount td) (\x -> x - 1)
     where
-        handle = serve cacheEnabled logEnabled
-
-        cleanUp = do
-            count <- takeMVarT =<< viewCount
-            putMVarT (count - 1) =<< viewCount
-            pure ()
+        client = _threadHandle td
 
 
-getQuery :: ServerApp (Maybe ByteString)
--- ^ Read a client query from our network handle
-getQuery = viewHandle >>= liftIO . protoRead
-
-
-serve :: Bool -> Bool -> ByteString -> ServerApp ()
+serve :: ThreadData -> ByteString -> IO ()
 -- ^ Run a user query through the database, and send them the result.
 -- If the database reports that it changed, we set writeNeeded.
-serve cacheEnabled logEnabled rawQuery = do
+serve (ThreadData client d w cc c o) rawQuery = do
 
-        cache <- readMVarT =<< viewCache
-
-        case get cacheEnabled cache query of
+        cache <- readTVarIO c
+        case get (_enableCache o) cache query of
 
             -- cache hit
             Just value -> do
-                replyToClient value =<< viewHandle
-                when logEnabled $
-                    logToConsole cacheHit noChange query
+                reply client value
+                when (_enableLog o) $
+                    logToConsole cc cacheHit noChange query
 
             -- cache miss, or disabled
             Nothing -> do
-                db <- takeMVarT =<< viewDatabase
+                value <- atomically $ do
+                    db <- readTVar d
 
-                let (result, changed, newDB) = runAction db query
-                    newCache = put cache query result
+                    let (result, changed, newDB) = runAction db query
+                        newCache = put cache query result
 
-                putMVarT newDB    =<< viewDatabase
-                _ <- takeMVarT =<< viewCache
-                putMVarT newCache =<< viewCache
+                    writeTVar d newDB
 
-                replyToClient result =<< viewHandle
-                when logEnabled $
-                    logToConsole cacheMiss noChange query
+                    if changed
+                        -- set writeNeeded, clear the cache
+                        then writeTVar w True >> writeTVar c emptyCache
 
-                when changed $ do
-                    setWriteNeeded
-                    clearCache
+                        -- no change, just update the cache
+                        else writeTVar c newCache
+
+                    pure result
+
+                reply client value
+                when (_enableLog o) $
+                    logToConsole cc cacheMiss noChange query
+
     where
         query :: Query
-        query = filter (not . T.null)
-              . T.split (== '\n')
+        query = filter (not . T.null) . T.split (== '\n')
               $ decodeUtf8 rawQuery
 
-        cacheHit = True
-        cacheMiss = False
-        noChange = False
+        reply :: Handle -> Text -> IO ()
+        reply h value = protoSend h $ encodeUtf8 value
+
+        (cacheHit, cacheMiss, noChange) = (True, False, False)
 
 
-setWriteNeeded :: ServerApp ()
-setWriteNeeded = do
-    _ <- takeMVarT =<< viewWrite
-    putMVarT True =<< viewWrite
-
-
-clearCache :: ServerApp ()
-clearCache = do
-    _ <- takeMVarT =<< viewCache
-    putMVarT emptyCache =<< viewCache
-
-
-replyToClient :: Text -> Handle -> ServerApp ()
-replyToClient value h = liftIO . protoSend h . encodeUtf8 $ value
-
-
-logToConsole :: Bool -> Bool -> Query -> ServerApp ()
+logToConsole :: ClientCount -> Bool -> Bool -> Query -> IO ()
 -- ^ write a summary of the query to stdout
-logToConsole hit write query = do
-        count <- readMVarT =<< viewCount
-        echoLocal . T.take 80 $
+logToConsole cc hit write query = do
+        count <- readTVarIO cc
+        putStrLn . T.unpack . T.take 80 $
             clients count <> status <> T.unwords query
     where
         status
@@ -241,39 +228,3 @@ logToConsole hit write query = do
             | count < 250 = "O"
             | count < 450 = "0"
             | otherwise   = "!"
-
-
--- | MVar Utilities
-
-putMVarT :: a -> MVar a -> ReaderT ThreadData IO ()
-putMVarT thing place = liftIO $ putMVar place thing
-
-readMVarT :: MVar a -> ReaderT ThreadData IO a
-readMVarT = liftIO . readMVar
-
-takeMVarT :: MVar a -> ReaderT ThreadData IO a
-takeMVarT = liftIO . takeMVar
-
-
--- | ReaderT Utilities
-
-viewHandle :: ReaderT ThreadData IO Handle
-viewHandle = asks _threadHandle
-
-viewDatabase :: ReaderT ThreadData IO Database
-viewDatabase = asks _database
-
-viewWrite :: ReaderT ThreadData IO WriteNeeded
-viewWrite = asks _writeNeeded
-
-viewCount :: ReaderT ThreadData IO ClientCount
-viewCount = asks _clientCount
-
-viewCache :: ReaderT ThreadData IO DbCache
-viewCache = asks _cache
-
-viewOptions :: ReaderT ThreadData IO Options
-viewOptions = asks _options
-
-echoLocal :: Text -> ReaderT ThreadData IO ()
-echoLocal = liftIO . putStrLn . T.unpack
